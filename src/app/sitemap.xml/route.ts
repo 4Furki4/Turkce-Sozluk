15f9bc628be5dd87f64722d281a03dba17edb3b3
Furkan Cengiz <muhammedcengiz1@gmail.com>
2025-07-28 16:45:28 +0900

    fix: update sitemap URLs to use /arama instead of /search for Turkish routes

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -3,21 +3,21 @@
 import { getBaseUrl } from '@/src/lib/seo-utils';
 
 export async function GET() {
-    const baseUrl = getBaseUrl();
-    const lastmod = new Date().toISOString();
+  const baseUrl = getBaseUrl();
+  const lastmod = new Date().toISOString();
 
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-    xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
-    // 1. Point to the static sitemap
-    xml += `<sitemap><loc>${baseUrl}/sitemap-static.xml</loc><lastmod>${lastmod}</lastmod></sitemap>\n`;
+  // 1. Point to the static sitemap
+  xml += `<sitemap><loc>${baseUrl}/sitemap-static.xml</loc><lastmod>${lastmod}</lastmod></sitemap>\n`;
 
-    // 2. Point to the sitemap-words index (which Next.js generates for you)
-    xml += `<sitemap><loc>${baseUrl}/sitemap-words-index.xml</loc><lastmod>${lastmod}</lastmod></sitemap>\n`;
+  // 2. Point to the sitemap-words index (which Next.js generates for you)
+  xml += `<sitemap><loc>${baseUrl}/sitemap-words-index.xml</loc><lastmod>${lastmod}</lastmod></sitemap>\n`;
 
-    xml += '</sitemapindex>';
+  xml += '</sitemapindex>';
 
-    return new Response(xml, {
-        headers: { 'Content-Type': 'application/xml' },
-    });
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
 }
