9e3977b2f040f5bd9bb4c4a9808dd6512970be1a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2026-01-06 12:09:07 +0900

    fix: Prevent sidebar globe icon from shrinking by adding `flex-shrink-0` class.

--- src/components/customs/sidebar.tsx
+++ src/components/customs/sidebar.tsx
@@ -113,7 +113,7 @@ export default function Sidebar(
                             </li>
                             <li>
                                 <NextIntlLink className='flex items-center gap-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 rounded-sm' href={'/foreign-term-suggestions'} onClick={() => setIsSidebarOpen(false)}>
-                                    <Globe className="h-6 w-6" /> <span className={`text-nowrap`}>{t("Navbar.ForeignTermSuggestions")}</span>
+                                    <Globe className="h-6 w-6 flex-shrink-0" /> <span className={`text-nowrap`}>{t("Navbar.ForeignTermSuggestions")}</span>
                                 </NextIntlLink>
                             </li>
                             <Separator />
