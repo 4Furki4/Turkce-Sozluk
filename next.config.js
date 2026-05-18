caa15b91ca48820e7e5dbb5fba766facb7c9aab4
Furkan Cengiz <muhammedcengiz1@gmail.com>
2026-05-18 12:12:24 +0300

    Prepare Raspberry Pi deployment configs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     htmlLimitedBots: /.*/,
     images: {
         remotePatterns: [
